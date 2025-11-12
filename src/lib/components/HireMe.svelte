8660aee4b93e6dcb235f61b388248fa7621a893f
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-11-11 16:02:18 -0800

    fix: resume button looks right now plus some extra styling

--- src/lib/components/HireMe.svelte
+++ src/lib/components/HireMe.svelte
@@ -6,13 +6,13 @@
 	</p>
     <p class="text-gruvbox-bg0 dark:text-gruvbox-fg0">
         If you're hiring or know of a role that might be a good fit, I'd love to hear from you!
-        You can <a>contact</a> me or check out my resume below.
+        You can <a href="" class="text-gruvbox-purple0 dark:text-gruvbox-orange1">contact</a> me or check out my resume below.
 	</p>
-    <div class="border">
-        <a href="resume-website.pdf" target="_blank" class="box-border px-4 py-2">
-            <img src="linklight.png" alt="linklight" class="inline-block h-[1em] w-auto pb-[2px] hidden dark:block" />
-            <img src="linkdark.png" alt="linkdark" class="inline-block h-[1em] w-auto pb-[2px] block dark:hidden" />
-            Resume
+    <div class="border border-gruvbox-bg0 dark:border-gruvbox-fg0 bg-linear-to-r from-gruvbox-purple0 dark:from-gruvbox-orange1 to-gruvbox-red0">
+        <a href="resume-website.pdf" target="_blank" class="box-border px-4 py-2 flex items-center gap-2">
+            <img src="linklight.png" alt="linklight" class="h-[1em] w-auto pb-[2px] hidden dark:block" />
+            <img src="linkdark.png" alt="linkdark" class="h-[1em] w-auto pb-[2px] block dark:hidden" />
+            <span class="font-bold text-gruvbox-bg0 dark:text-gruvbox-fg0">Resume</span>
         </a>
     </div>
 </div>
