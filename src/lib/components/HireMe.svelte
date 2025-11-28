65003561981e021b2cb017ef57147d5cd80cb8ee
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-11-27 19:17:13 -0800

    fix: project card no longer fills entire row

--- src/lib/components/HireMe.svelte
+++ src/lib/components/HireMe.svelte
@@ -1,4 +1,9 @@
-<div class="flex flex-col items-start bg-gruvbox-fg1 dark:bg-gruvbox-bg1 border border-dashed border-gruvbox-bg4 dark:border-gruvbox-fg4 box-border p-4 gap-4">
+<div class="
+    flex flex-col items-start
+    bg-gruvbox-fg1 dark:bg-gruvbox-bg1
+    border border-dashed border-gruvbox-bg4 dark:border-gruvbox-fg4 box-border
+    p-4 gap-4
+">
 	<span>🛈 <span class="font-extrabold">README</span></span>
     <p>
 		I'm currently looking for a summer 2026 internship where I can explore and apply my
@@ -8,7 +13,10 @@
         If you're hiring or know of a role that might be a good fit, I'd love to hear from you!
         You can contact me or check out my resume below.
 	</p>
-    <div class="border border-gruvbox-bg0 dark:border-gruvbox-fg0 bg-linear-to-r from-gruvbox-blue0 dark:from-gruvbox-green1 to-gruvbox-purple0 dark:to-gruvbox-red0">
+    <div class="
+        border border-gruvbox-bg0 dark:border-gruvbox-fg0
+        bg-linear-to-r from-gruvbox-blue0 dark:from-gruvbox-green1 to-gruvbox-purple0 dark:to-gruvbox-red0
+    ">
         <a href="resume-website.pdf" target="_blank" class="box-border px-4 py-2 flex items-center gap-2">
             <img src="linklight.png" alt="linklight" class="h-[1em] w-auto pb-[2px] hidden dark:block" />
             <img src="linkdark.png" alt="linkdark" class="h-[1em] w-auto pb-[2px] block dark:hidden" />
--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -2,7 +2,16 @@
     let { title, description, image } = $props();
 </script>
 
-<div class="bg-gruvbox-fg1 dark:bg-gruvbox-bg1 border border-dashed border-gruvbox-bg4 dark:border-gruvbox-fg4 box-border p-4 gap-4">
-    {title}
-    {description}
+<div class="
+    flex flex-col
+    bg-gruvbox-fg1 dark:bg-gruvbox-bg1 
+    border border-dashed border-gruvbox-bg4 dark:border-gruvbox-fg4 box-border 
+    p-4 gap-2 h-full
+">
+    <div class="font-bold text-gruvbox-blue0 dark:text-gruvbox-green1">
+        {title}
+    </div>
+    <div class="text-gruvbox-bg0 dark:text-gruvbox-fg0">
+        {description}
+    </div>
 </div>
